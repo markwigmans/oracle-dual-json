e9542a47f073f326f33698d4760e7f3fb0faab2d
buildingsoftwareblocks <mark.wigmans@gmail.com>
2023-11-16 21:13:57 +0100

    Add driver_dv

--- src/main/sql/driverJsonView.sql
+++ src/main/sql/driverJsonView.sql
@@ -1,1 +1,21 @@
-
+CREATE OR REPLACE JSON RELATIONAL DUALITY VIEW driver_dv AS
+SELECT JSON {
+        'driverId' : d.id,
+    'name' : d.NAME,
+    'points' : d.POINTS,
+    UNNEST (SELECT JSON {
+    'teamId' : t.ID,
+    'team' : t.NAME
+    }
+    FROM TEAM t
+    WHERE d.TEAM_ID = t.ID),
+    'race' :[ SELECT JSON { 'positionId' : pp.ID,
+                  UNNEST (	SELECT JSON {'raceId' : r.ID, 'name' : r.NAME, 'country' : r.COUNTRY}
+                  FROM RACE r WHERE r.ID = pp.RACE_ID),
+                  'position' : pp."POSITION"
+                  }
+              FROM PODIUM_POSITION pp
+              WHERE pp.DRIVER_ID = d.ID
+                  ]
+                  }
+              FROM DRIVER d
