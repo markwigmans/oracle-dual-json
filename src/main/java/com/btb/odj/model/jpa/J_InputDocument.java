81ba32169a82eb643b3e3234a4d358a83f2eff76
buildingsoftwareblocks <mark.wigmans@gmail.com>
2023-12-29 21:26:21 +0100

    first version JPA processor

---
+++ src/main/java/com/btb/odj/model/jpa/J_InputDocument.java
@@ -0,0 +1,19 @@
+package com.btb.odj.model.jpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Immutable;
+
+@Entity
+@Table(name = "InputDocument")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Immutable
+public class J_InputDocument extends J_AbstractEntity {
+
+    private String data;
+}
---
+++ src/main/java/com/btb/odj/repository/jpa/J_InputRepository.java
@@ -0,0 +1,5 @@
+package com.btb.odj.repository.jpa;
+
+import com.btb.odj.model.jpa.J_InputDocument;
+
+public interface J_InputRepository extends J_AbstractRepository<J_InputDocument> {}
