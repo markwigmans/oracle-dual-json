b347beb07719d63993d447af671180e61504e575
buildingsoftwareblocks <mark.wigmans@gmail.com>
2024-01-27 20:19:30 +0100

    Add full provider support

--- src/main/java/com/btb/odj/service/RequestService.java
+++ src/main/java/com/btb/odj/service/RequestService.java
@@ -2,8 +2,11 @@ package com.btb.odj.service;
 
 import static com.btb.odj.util.Provider.*;
 
+import com.btb.odj.service.provider.ProviderProperties;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -15,18 +18,22 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class RequestService {
 
-    private final ESDataService esDataService;
-    private final JPADataService jpaDataService;
-    private final MongoDataService mongoDataService;
+    private final Optional<ESDataService> esDataService;
+    private final Optional<JPADataService> jpaDataService;
+    private final Optional<MongoDataService> mongoDataService;
+    private final ProviderProperties properties;
 
     @SneakyThrows
     public Map<String, List<?>> findDriversWithMoreThan(int points) {
-        CompletableFuture<? extends List<?>> futureM =
-                CompletableFuture.supplyAsync(() -> mongoDataService.findDriversWithMoreThan(points));
-        CompletableFuture<? extends List<?>> futureE =
-                CompletableFuture.supplyAsync(() -> esDataService.findDriversWithMoreThan(points));
-        CompletableFuture<? extends List<?>> futureJ =
-                CompletableFuture.supplyAsync(() -> jpaDataService.findDriversWithMoreThan(points));
+        final var futureM = properties.isActive(MongoDB)
+                ? CompletableFuture.supplyAsync(() -> mongoDataService.get().findDriversWithMoreThan(points))
+                : CompletableFuture.completedFuture(Collections.emptyList());
+        final var futureE = properties.isActive(ElasticSearch)
+                ? CompletableFuture.supplyAsync(() -> esDataService.get().findDriversWithMoreThan(points))
+                : CompletableFuture.completedFuture(Collections.emptyList());
+        final var futureJ = properties.isActive(JPA)
+                ? CompletableFuture.supplyAsync(() -> jpaDataService.get().findDriversWithMoreThan(points))
+                : CompletableFuture.completedFuture(Collections.emptyList());
 
         CompletableFuture.allOf(futureM, futureE, futureJ).join();
         return Map.of(MongoDB.label, futureM.join(), ElasticSearch.label, futureE.join(), JPA.label, futureJ.join());
--- src/main/java/com/btb/odj/service/provider/ProviderProperties.java
+++ src/main/java/com/btb/odj/service/provider/ProviderProperties.java
@@ -1,16 +1,26 @@
 package com.btb.odj.service.provider;
 
+import com.btb.odj.util.Provider;
+import java.util.Arrays;
 import java.util.List;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
-@Setter
 @Configuration
 @ConfigurationProperties(prefix = "odj.db")
 public class ProviderProperties {
 
-    private List<String> providers;
+    private List<Provider> providers;
+
+    public void setProviders(List<String> list) {
+        this.providers = Arrays.stream(Provider.values())
+                .filter(p -> list.contains(p.label))
+                .toList();
+    }
+
+    public boolean isActive(Provider provider) {
+        return providers.contains(provider);
+    }
 }
