53a8cfade9dad1fa67f714e6e45a91b4fe640408
The Builder <noreply@github.com>
2024-07-22 18:35:20 +0000

    committed formatting changes

--- src/test/java/com/btb/odj/service/DataRaceServiceTest.java
+++ src/test/java/com/btb/odj/service/DataRaceServiceTest.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 @Import(TestContainersConfig.class)
